cb6d9660ba042b838e1aced14dca3fb52add0aea
Daniel Oskarsson <daniel.oskarsson@gmail.com>
2015-11-19 07:14:47 +0100

    Add Store API Client Constants

---
+++ java/src/main/java/org/vasttrafik/wso2/carbon/apimgt/store/api/clients/Constants.java
@@ -0,0 +1,14 @@
+package org.vasttrafik.wso2.carbon.apimgt.store.api.clients;
+
+/**
+ * @author Daniel Oskarsson <daniel.oskarsson@gmail.com>
+ */
+interface Constants {
+
+    String ACTION = "action";
+    String TIER_UNLIMITED = "Unlimited";
+    String KEYTYPE_PRODUCTION = "PRODUCTION";
+    String DEFAULT_APPLICATION = "DefaultApplication";
+    String AUTHORIZED_DOMAINS_ALL = "ALL";
+
+}
