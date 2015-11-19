40b98a33b0d71e2d33b856976fb8c7ddd25c3c0a
Daniel Oskarsson <daniel.oskarsson@gmail.com>
2015-11-19 07:02:05 +0100

    Reformat tabs -> spaces

--- java/src/test/java/org/vasttrafik/wso2/carbon/apimgt/portal/api/PortalAPIIT.java
+++ java/src/test/java/org/vasttrafik/wso2/carbon/apimgt/portal/api/PortalAPIIT.java
@@ -1,27 +1,29 @@
 package org.vasttrafik.wso2.carbon.apimgt.portal.api;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
 import static org.junit.Assert.assertEquals;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 public class PortalAPIIT {
 
-	private static WebTarget target;
+    private static WebTarget target;
 
-	@BeforeClass
-	public static void beforeClass() {
-		final String endpointUrl = System.getProperty("service.url");
-		target = ClientBuilder.newClient().target(endpointUrl);
-	}
+    @BeforeClass
+    public static void beforeClass() {
+        final String endpointUrl = System.getProperty("service.url");
+        target = ClientBuilder.newClient().target(endpointUrl);
+    }
 
-	@Test
-	public void testGetTier() throws Exception {
-		final Response reponse = target.path("/api/tiers").request().accept(MediaType.APPLICATION_JSON).get();
-		assertEquals(Response.Status.OK.getStatusCode(), reponse.getStatus());
-	}
+    @Test
+    public void testGetTier() throws Exception {
+        final Response reponse = target.path("/api/tiers").request().accept(MediaType.APPLICATION_JSON).get();
+        assertEquals(Response.Status.OK.getStatusCode(), reponse.getStatus());
+    }
 
 }
