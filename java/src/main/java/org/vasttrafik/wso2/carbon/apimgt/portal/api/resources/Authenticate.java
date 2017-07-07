c91d54d15a6440dfb12ce3810848d76be7a6961c
Christian Morin <christian@fourmation.se>
2017-07-07 13:35:30 +0200

    Added TOTP ability for authentication.

--- java/src/main/java/org/vasttrafik/wso2/carbon/apimgt/portal/api/resources/Authenticate.java
+++ java/src/main/java/org/vasttrafik/wso2/carbon/apimgt/portal/api/resources/Authenticate.java
@@ -11,6 +11,7 @@ import javax.ws.rs.ServerErrorException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.jboss.aerogear.security.otp.Totp;
 import org.vasttrafik.wso2.carbon.apimgt.portal.api.security.Security;
 import org.vasttrafik.wso2.carbon.common.api.utils.ResponseUtils;
 import org.vasttrafik.wso2.carbon.identity.api.utils.UserAdminUtils;
@@ -19,46 +20,74 @@ import org.vasttrafik.wso2.carbon.identity.oauth.authcontext.JWTTokenGenerator;
 import org.vasttrafik.wso2.carbon.common.api.beans.AuthenticatedUser;
 import org.vasttrafik.wso2.carbon.common.api.beans.Credentials;
 
-
 @Path("/authenticate")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public final class Authenticate extends PortalResource {
-	
+
 	private static JWTTokenGenerator tokenGenerator = new JWTTokenGenerator();
-	
+
 	@POST
-    public Response postAuthenticate(@Valid final Credentials credentials) 
-    	throws ServerErrorException 
-    {
+	public Response postAuthenticate(@Valid final Credentials credentials) throws ServerErrorException {
 		try {
 			// Get username
 			String userName = credentials.getUserName();
-			
+
 			if ("admin".equalsIgnoreCase(userName)) {
 				return Response.status(Response.Status.FORBIDDEN).build();
 			}
-				
+
 			// Authenticate the user
-		    UserAdminUtils.authenticateCredentials(userName, credentials.getCredential());
-		    // Login the user
-		    Security.login(credentials);
+			UserAdminUtils.authenticateCredentials(userName, credentials.getCredential());
+
+			// Authenticating via TOTP
+			if (credentials.getTotp() != null) {
+				Totp totp = new Totp(
+						UserAdminUtils.getUserClaimValue(userName, "http://wso2.org/claims/secretKey", "default"));
+				boolean isCodeValid = (totp.now().equals(credentials.getTotp()));
+
+				if (isCodeValid) {
+					// Login the user
+					Security.login(credentials);
+
+					// Generate a token
+					JWTToken jwtToken = tokenGenerator.generateToken(userName);
+					// Create the response object
+					AuthenticatedUser user = new AuthenticatedUser(jwtToken);
+					// Create the response object
+					return Response.status(201).entity(user).build();
+				} else {
+					return Response.status(Response.Status.UNAUTHORIZED).entity("{\"message\":\"Incorrect TOTP\"}").build();
+				}
+			}
 			
+			String enabledTotp = UserAdminUtils.getUserClaimValue(userName, "http://wso2.org/claims/enableTOTP", "default");
+
+			if (enabledTotp != null && enabledTotp.equalsIgnoreCase("true")) {
+
+				AuthenticatedUser authenticatedUser = new AuthenticatedUser();
+				authenticatedUser.setUserId(UserAdminUtils.getUserId(userName));
+				authenticatedUser.setUserName(userName);
+				authenticatedUser.setEnabledTotp(true);
+
+				// Create the response object
+				return Response.status(201).entity(authenticatedUser).build();
+			}
+
+			// Login the user
+			Security.login(credentials);
+
 			// Generate a token
 			JWTToken jwtToken = tokenGenerator.generateToken(userName);
 			// Create the response object
 			AuthenticatedUser user = new AuthenticatedUser(jwtToken);
 			// Create the response object
 			return Response.status(201).entity(user).build();
-		}
-		catch(NotAuthorizedException | InternalServerErrorException ie) {
-			return Response.status(Response.Status.UNAUTHORIZED)
-				.entity(ie.getCause())
-				.build();
-		}
-		catch (Exception e) {
+		} catch (NotAuthorizedException | InternalServerErrorException ie) {
+			return Response.status(Response.Status.UNAUTHORIZED).entity(ie.getCause()).build();
+		} catch (Exception e) {
 			Response response = ResponseUtils.serverError(e);
 			throw new ServerErrorException(response);
 		}
-    }
+	}
 }
